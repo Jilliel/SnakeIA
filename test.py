68bffa50112c0233c329e2a1732edd5dc2262f10
Raphael Dalle Risbourg <raphael.dalle@telecom-paris.fr>
2025-04-22 17:14:26 +0200

    Maj test.py

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
 from bot.randy import RandomSnake
+from bot.student import CleverSnake
 
-snake = RandomSnake(width=15, height=15)
-snake.debug = True
-snake.run()
+def testRandSnake():
+    snake = RandomSnake(width=15, height=15)
+    snake.debug = True
+    snake.run()
+
+def testCleverSnake():
+    snake = CleverSnake()
+    snake.debug = True
+    snake.run()
+
+if __name__ == "__main__":
+    #testRandSnake()
+    testCleverSnake()
