1dc6c558021c03b50227d2395effc27e3afb11e5
Raphael Dalle Risbourg <raphael.dalle@telecom-paris.fr>
2025-04-23 13:47:03 +0200

    Ajout d'une statistique pour la durée de la partie

--- train.py
+++ train.py
@@ -13,20 +13,30 @@ snake.load("weights.pth")
 simsize = 100
 epochsize = 50
 
-buffer = deque([], maxlen=epochsize)
-history = []
+score_buffer = deque([], maxlen=epochsize)
+round_buffer = deque([], maxlen=epochsize)
+score_history = []
+round_history = []
 
 for i in range(simsize):
     for _ in range(epochsize):
         snake.run()
-        buffer.append(snake.score)
-    accuracy = sum(buffer)/epochsize
-    print(f"Epoch {i}: accuracy={accuracy}")
-    history.append(accuracy)
+        score_buffer.append(snake.score)
+        round_buffer.append(snake.round)
+    mscore = sum(score_buffer)/epochsize
+    mround = sum(round_buffer)/epochsize
+    print(f"Epoch {i}:\n\t-Score moyen={mscore}\n\t-Durée moyenne={mround} rounds.")
+    score_history.append(mscore)
+    round_history.append(mround)
 
-plt.plot(history)
+plt.subplot(1, 2, 1)
+plt.plot(score_history)
 plt.xlabel("Epoch")
 plt.ylabel("Score moyen")
-plt.savefig("history.png")
+plt.subplot(1, 2, 2)
+plt.plot(round_history)
+plt.xlabel("Epoch")
+plt.ylabel("Nombre de rounds moyen")
 
+plt.savefig("history.png")
 snake.save("weights.pth")
