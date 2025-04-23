ea2e01270f9b67f9bf22c6fa7dad4ec66eed9f85
Raphael Dalle Risbourg <raphael.dalle@telecom-paris.fr>
2025-04-23 20:34:39 +0200

    Maj test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from bot.randy import RandomSnake
 from bot.clever import CleverSnake
-
+from learning.training import Qtrainer
 def test_random():
     snake = RandomSnake()
     snake.debug = True
@@ -10,3 +10,8 @@ def test_clever():
     snake = CleverSnake()
     snake.debug = True
     snake.run()
+
+def test_train():
+    snake = CleverSnake()
+    trainer = Qtrainer(snake)
+    trainer.play()
